a575c25423808d70f711370d053e1256b13d2eb3
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2023-05-13 12:31:28 -0500

    Completed a backtracking problem and added comments to link lists merge problem

---
+++ combination_sum_dfs.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        # O(t) efficency is 2^(t) where t = target
+        def dfs(i, current, total):
+            # base cases
+            if total  == target:
+                res.append(list(current))  # append a copy
+                # res.append(current)
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            # recursisve steps
+
+            # case where candidates[i] is included in combo
+            current.append(candidates[i])
+            dfs(i, current, total + candidates[i])
+
+            # case wehre candidates[i] is NOT included in combo
+            current.pop()
+            dfs(i + 1, current, total)
+            # returns implicitly
+        
+        # start dfs algorithm
+        dfs(0, [], 0)
+        return res
--- merge_linked_lists.py
+++ merge_linked_lists.py
@@ -18,30 +18,30 @@ class Solution(object):
         elif (not list1 == None) and (list2 == None):
             return list1
         
-        return_list = ListNode()  # empty node that will be thrown away at the end
-        return_head = return_list
-        
-        
-        while (not list1 == None) or (not list2 == None):
 
+        return_list = ListNode()  # empty node that will be thrown away at the end
+        return_head = return_list  # save the head of the return_list for the very end
+        
+        # while at least one of them is not a None pointer, add nodes one at a time
+        while (not list1 == None) or (not list2 == None):
+            # check if one of the pointers is None
             if (list1 == None) and (not list2 == None):
-                return_list.next = ListNode(list2.val)
-                list2 = list2.next
-                
+                return_list.next = ListNode(list2.val)  # copy value that list2 points to
+                list2 = list2.next  # advance list2 pointer
             elif (not list1 == None) and (list2 == None):
                 return_list.next = ListNode(list1.val)
                 list1 = list1.next
             
-            elif list1.val <= list2.val:
+            # if neither them point to None, append the lesser value to return_list
+            elif list1.val <= list2.val: 
                 return_list.next = ListNode(list1.val)
                 list1 = list1.next
-                
             elif list2.val < list1.val:
                 return_list.next = ListNode(list2.val)
                 list2 = list2.next
            
-            return_list = return_list.next
+            return_list = return_list.next  # advance return_list pointer for next iteration
 
         
-        return_head = return_head.next  # fix head
+        return_head = return_head.next  # discard empty node that was used for initialization
         return return_head
